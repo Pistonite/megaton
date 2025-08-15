bfe52b0469cfde0c2e9a0b0ae0701eb3a821f47f
Pistonight <pistonknight@outlook.com>
2025-08-15 00:31:16 -0700

    unfuck licenses

--- lib/src/nx/README.md
+++
@@ -1,1 +0,0 @@
-Sources of some parts of libnx is included because it's incompatible to link with libnx in our use case.
--- packages/clib/include/megaton/__priv/proc_handle.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <switch/types.h>
-
-namespace megaton::__priv {
-/** Get the handle for the current process. */
-Handle current_process();
-} // namespace megaton::__priv
