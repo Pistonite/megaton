8ee23e2f9500a3535fb077147a532038c30aa019
Michael Chalupiak <mikec@mchalupiak.com>
2025-10-26 14:48:40 -0400

    Added code for malloc

---
+++ packages/abi/cxx/include/alloc.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <switch/types.h>
+#define BSS_ALLOC_SIZE 0x20000
+
+namespace nn::mem {
+    class StandardAllocator {
+        public:
+            StandardAllocator();
+            // StandardAllocator(void* address, size_t size);
+            // StandardAllocator(void* address, size_t size, bool enableCache);
+
+            ~StandardAllocator() {
+                if (mIsInitialized) {
+                    Finalize();
+                }
+            }
+
+            void Initialize(void* address, size_t size);
+            void* Allocate(size_t size);
+            void* Allocate(size_t size, size_t alignment);
+            void Free(void* address);
+            bool mIsInitialized;
+            void Finalize();
+    };
+}
