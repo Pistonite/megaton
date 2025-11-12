b5d5ce18e60b8e6b1adae469f79e9cc2cf3b5e44
Michael Chalupiak <mikec@mchalupiak.com>
2025-11-12 14:08:33 -0500

    updating to statically initialized btreemap

--- packages/abi/cxx/src/abort.cpp
+++ packages/abi/cxx/src/abort.cpp
@@ -4,10 +4,7 @@
 #include <toolkit/tcp.hpp>
 #include <switch/types.h>
 
-extern "C" void init_env();
-
 extern "C" void sys_abort() {
-    init_env();
     botw::tcp::sendf("aborting due to panic in new library!\n");
     u8* ptr = (u8*) 0xFFFFFFFFFFFFFFFF;
     u8 x = *ptr; // crash should happen here (ideally)
