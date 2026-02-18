487aa6dd3452da7b029db432e8b2a1ebbcc81dce
Michael Chalupiak <54217969+mchalupiak@users.noreply.github.com>
2026-02-18 15:42:43 -0500

    Feat/update target dir (#73)
    * Added changes necessary to update target directory to specification
    
    * Updated cxxbridge code to reflect documented target directory, also backported
    some changes from async refactor to make this easier to merge in the future
    
    * Ran cargo fmt (for ci/cd)
    
    * Fix checks
    
    * Updated environment to include CC, CXX, AR variables
    
    * Make checks happy
    
    ---------
    
    Co-authored-by: Michael Chalupiak <mikec@mchalupiak.com>

--- packages/lib/src/alloc.cpp
+++ packages/lib/src/alloc.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (c) 2025-2026 Megaton contributors
 
-#include <__internal/alloc.h>
+#include <megaton/__internal/alloc.h>
 // #include <toolkit/tcp.hpp>
 
 static u8 bss_alloc[BSS_ALLOC_SIZE] = {0};
