c6e90f119205e620e02aef10fccd7709cd8e2dfd
Michael Chalupiak <mikec@mchalupiak.com>
2026-04-29 16:21:25 -0400

    ported test mod

---
+++ packages/test-mod/scripts/lftp-upload.sh
@@ -0,0 +1,6 @@
+mkdir -p -f /atmosphere/contents/01007EF00011E000/exefs
+cd /atmosphere/contents/01007EF00011E000/exefs
+mput -e target/megaton/none/example.nso
+mput -e target/megaton/none/main.npdm
+rm -f subsdk9
+mv example.nso subsdk9
