c79f7b7358a346213679b59177d98be3e1a5e94b
Andrew Petersen <realandrewpetersen@gmail.com>
2026-05-20 18:39:56 -0400

    Add license line

--- packages/test-mod/src/fs_tests.rs
+++ packages/test-mod/src/fs_tests.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2025-2026 Megaton contributors
+
 use std::{fs::{File, OpenOptions}, io::{Read, Write}, path::PathBuf};
 
 use crate::MegatonTests;
